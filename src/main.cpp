81b3534b0bd240124e6c5ffd74f8b252f1cf1eb0
AceTheSoulKing <tensazangetsu741@gmail.com>
2026-05-17 15:21:21 +0530

    feat: complete main.cpp with db init, login, and main window launch

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
 #include <QApplication>
-#include <QLabel>
+
+#include "db/Database.h"
+#include "db/Schema.h"
+#include "db/StockRepository.h"
+#include "ui/LoginDialog.h"
+#include "ui/MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QLabel label("TradeSim - Hello World");
-    label.show();
+
+    if (!Database::instance().open())
+    {
+        return 1;
+    }
+
+    if (!Schema::createTables())
+    {
+        return 1;
+    }
+
+    StockRepository::seedStocks();
+
+    LoginDialog loginDialog;
+    if (loginDialog.exec() != QDialog::Accepted)
+    {
+        return 0;
+    }
+
+    auto user = loginDialog.getLoggedInUser();
+    if (!user)
+    {
+        return 0;
+    }
+
+    MainWindow mainWindow(*user);
+    mainWindow.show();
+
     return app.exec();
 }
